0be8aaa973efd92b569439afd0b0b1f1b67fa3dc
hayk ferayan <hayk715@gmail.com>
2018-08-18 11:46:41 -0700

    files

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "reserved.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/add", function(req, res) {
